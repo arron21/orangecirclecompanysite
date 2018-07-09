875533e9df46bf7351ddc58339324f826d1a97a8
arron <amccrory@citizenhawk.com>
2018-07-09 13:50:30 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,69 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HomeCodingComponent } from './home-coding/home-coding.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { ArtSquareComponent } from './art-components/art-square/art-square.component';
+import { WebsitesComponent } from './orange-services/websites/websites.component';
+import { PhotographyComponent } from './orange-services/photography/photography.component';
+import { AdwordsComponent } from './orange-services/adwords/adwords.component';
+import { SeoComponent } from './orange-services/seo/seo.component';
+import { ConsultingComponent } from './orange-services/consulting/consulting.component';
+import { CodingComponent } from './orange-services/coding/coding.component';
+const appRoutes: Routes = [
+    { path: '', component: HomePageComponent, pathMatch: 'full' },
+    { path: 'about', component: AboutUsComponent},
+    {
+        path: 'pricing',
+        loadChildren: './pricing/pricing.module#PricingModule',
+        data: {preload: true}
+
+    },
+    { path: 'contact', component: ContactComponent},
+    { path: 'works',
+      loadChildren: './works/works.module#WorksModule',
+      data: {preload: true}
+    },
+    { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    AboutUsComponent,
+    ContactComponent,
+    HomeCodingComponent,
+    ArtSquareComponent,
+    WebsitesComponent,
+    PhotographyComponent,
+    AdwordsComponent,
+    SeoComponent,
+    ConsultingComponent,
+    CodingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
