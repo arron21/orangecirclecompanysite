bae4c7ffea9fa747335750529a7a6c209f060c5f
arron <amccrory@citizenhawk.com>
2018-07-31 22:43:36 -0700

    added angularitics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+import { Angulartics2GoogleTagManager } from 'angulartics2/gtm';
+import {Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(
+    angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
+    angulartics2GoogleTagManager: Angulartics2GoogleTagManager,
+    public router: Router
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // ga('set', 'page', event.urlAfterRedirects);
+        // ga('send', 'pageview');
+      }
+    });
+  }
 }
