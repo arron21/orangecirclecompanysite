2cb83ef168c6d4f29c5f47850db816c052f2c14e
arron <amccrory@citizenhawk.com>
2018-07-31 18:06:20 -0700

    bunch of changes

---
+++ src/app/shared/contact-btn/contact-btn.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-btn',
+  templateUrl: './contact-btn.component.html',
+  styleUrls: ['./contact-btn.component.scss']
+})
+export class ContactBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log('contact')
+  }
+
+}
