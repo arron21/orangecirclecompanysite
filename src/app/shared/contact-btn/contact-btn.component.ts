d934fb9df8996dd2a80196375ae788e3c44b952b
arron <amccrory@citizenhawk.com>
2018-08-25 13:50:33 -0700

    lots of changes

--- src/app/shared/contact-btn/contact-btn.component.ts
+++ src/app/shared/contact-btn/contact-btn.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '../../../../node_modules/@angular/router';
+import { Component, OnInit, Input, ElementRef, Renderer2 } from "@angular/core";
+import { Router } from "../../../../node_modules/@angular/router";
 
 @Component({
-  selector: 'app-contact-btn',
-  templateUrl: './contact-btn.component.html',
-  styleUrls: ['./contact-btn.component.scss']
+  selector: "app-contact-btn",
+  templateUrl: "./contact-btn.component.html",
+  styleUrls: ["./contact-btn.component.scss"]
 })
 export class ContactBtnComponent implements OnInit {
-
+  @Input("color") color;
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit() {
+    if (this.color) {
+      console.log(this.color);
+      console.log(this.el)
+      this.renderer.setStyle(this.el.nativeElement.children[0], 'background', this.color);
+    }
   }
 
   submit() {
-    console.log('contact');
-    this.router.navigate(['/contact']);
+    console.log("contact");
+    this.router.navigate(["/contact"]);
   }
-
 }
